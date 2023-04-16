4cfe7e5cb22d7efd9c7b76d82d70694d059f9bb3
Michael Napoli <mikednapoli@gmail.com>
2023-04-15 20:28:45 -0400

    initializing model and cost functions

--- root.py
+++ root.py
@@ -8,6 +8,13 @@ from numpy import random as rd
 import math
 import matplotlib.pyplot as plt
 
+
+# hyper parameters
+N = 10;
+PH = 5;
+
+
+# model and cost functions
 def model(x, u):
     F     = u[0];
     tauZ  = u[1];
@@ -27,3 +34,16 @@ def model(x, u):
     ];
 
     return dx;
+
+def cost(mpc_var, xlist, ulist):
+    xg = [0,0,1,0,0,0,0,0,0,0];
+    k = [10,10,10,1,1,1,1,1,1,1];
+
+    C = 0;
+    for x in xlist:
+        C += sum([k[i]*(x[i] - xg[i])**2 for i in range(N)]);
+
+    return C;
+
+if __name__ == "__main__":
+    pass;
