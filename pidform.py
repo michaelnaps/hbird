0d3b5da05b756d8f2226ccfe9d3fbe72a2562e3f
Michael Napoli <mikednapoli@gmail.com>
2023-04-20 14:40:12 -0400

    moved hyper paramters to root file: need to differentiate between cont Nx and discrete Nx

--- pidform.py
+++ pidform.py
@@ -1,123 +1,4 @@
-import sys
-sys.path.insert(0, '/home/michaelnaps/prog/mpc');
-
-
-import mpc
-import numpy as np
-from scipy.integrate import solve_ivp
-
-import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d as plt3d
-import matplotlib.patches as patch
-import matplotlib.path as path
-
-# suppress print statements
-np.set_printoptions(precision=5, suppress=True);
-
-
-# hyper parameters
-m = 1;
-g = 9.81;
-a = 1.00;  # discount rate
-eps = 0.1;
-Nx = 15;
-Nu = 3;
-dt = 0.01;
-
-
-# simulation vehicle entity
-class Vehicle:
-    def __init__(self, x0, xd, fig=None, axs=None, buffer_length=10, pause=1e-3):
-        # initialize figure properties
-        if fig is None:
-            self.fig = plt.figure();
-            self.axs = plt.axes(projection='3d');
-        else:
-            self.fig = fig;
-            self.axs = axs;
-
-        self.xd = xd;
-
-        self.axs.set_xlim3d(-10, 10);
-        self.axs.set_ylim3d(-10, 10);
-        self.axs.set_zlim3d(-10, 10);
-        # self.axs.axis('equal');
-        # self.fig.tight_layout();
-
-        self.axs.set_xlabel('x');
-        self.axs.set_ylabel('y');
-        self.axs.set_zlabel('z');
-
-        # initialize buffer (trail)
-        self.buffer = np.kron( np.ones( (buffer_length, 1) ), x0 );
-        self.trail_patch = patch.PathPatch(path.Path(self.buffer[:,0:2]));
-
-        # initialize force directions
-        x = x0[0];
-        y = x0[1];
-        z = x0[2];
-
-        self.Fx_patch = patch.Arrow(x, y, 4, 0, color='r');
-        self.Fy_patch = patch.Arrow(x, y, 0, 4, color='g');
-        self.Fz_patch = patch.Arrow(z, x, 0, 4, color='b');
-
-        self.axs.add_patch(self.Fx_patch);
-        self.axs.add_patch(self.Fy_patch);
-        self.axs.add_patch(self.Fz_patch);
-        self.axs.add_patch(self.trail_patch);
-
-        plt3d.art3d.pathpatch_2d_to_3d(self.Fx_patch, zdir='z');
-        plt3d.art3d.pathpatch_2d_to_3d(self.Fy_patch, zdir='z');
-        plt3d.art3d.pathpatch_2d_to_3d(self.Fz_patch, zdir='y');
-        plt3d.art3d.pathpatch_2d_to_3d(self.trail_patch, z=self.buffer[:,2]);
-
-        plt.show(block=0);
-        plt.close('all');
-
-        # plot pause
-        self.pause = pause;
-
-    def update(self, mvar, t, x, u):
-        # update trail
-        self.Fx_patch.remove();
-        self.Fy_patch.remove();
-        self.Fz_patch.remove();
-        self.trail_patch.remove();
-
-        self.buffer[:-1] = self.buffer[1:];
-        self.buffer[-1] = x;
-
-        xpos = x[0];
-        ypos = x[1];
-        zpos = x[2];
-        delta = x[3];
-        theta = x[4];
-
-        F = u[0];
-        Fx = F*np.cos(delta)*np.cos(theta);
-        Fy = F*np.cos(delta)*np.sin(theta);
-        Fz = F*np.sin(delta) - m*g;
-
-        self.Fx_patch = patch.Arrow(xpos, ypos, 4*Fx, 0, color='r');
-        self.Fy_patch = patch.Arrow(xpos, ypos, 0, 4*Fy, color='g');
-        self.Fz_patch = patch.Arrow(zpos, xpos, 0, 4*Fz, color='b');
-        self.trail_patch = patch.PathPatch(path.Path(self.buffer[:,0:2]), fill=0);
-
-        self.axs.add_patch(self.Fx_patch);
-        self.axs.add_patch(self.Fy_patch);
-        self.axs.add_patch(self.Fz_patch);
-        self.axs.add_patch(self.trail_patch);
-
-        plt3d.art3d.pathpatch_2d_to_3d(self.Fx_patch, zdir='z');
-        plt3d.art3d.pathpatch_2d_to_3d(self.Fy_patch, zdir='z');
-        plt3d.art3d.pathpatch_2d_to_3d(self.Fz_patch, zdir='y');
-        plt3d.art3d.pathpatch_2d_to_3d(self.trail_patch, z=self.buffer[:,2]);
-
-        plt.show(block=0);
-        plt.pause(self.pause);
-
-        return self;
-
+from root import *
 
 # model and cost functions
 def model(x, u):
@@ -181,8 +62,8 @@ def linearized(x, u, xd):
     return xplus.reshape(Nx,);
 
 def control(x):
-    k = [0.95, 0.05, 0];
     d = [0, 0, 0];
+    k = [0.95, 0.05, 0];
 
     u1 = d[0] - k[0]*x[2] - k[1]*x[7] - k[2]*x[12];
     u2 = d[1] - k[0]*x[3] - k[1]*x[8] - k[2]*x[13];
