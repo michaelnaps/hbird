59301ba8253b0c2977ad831a97b45a68a302bb46
Michael Napoli <mikednapoli@gmail.com>
2023-04-20 15:57:26 -0400

    dpa seems to be working for N=2

--- dpaform.py
+++ dpaform.py
@@ -1,5 +1,81 @@
 from root import *
 
+# class for dynamic programming
+class DynamicProgramming:
+    def __init__(self, pcost, tcost, model, N, Nx, Nu,
+        params=None, max_iter=10,
+        appx_zero=1e-6, grad_step=1e-3):
+        self.pcost = pcost;
+        self.tcost = tcost;
+        self.model = model;
+        self.params = params;
+
+        self.N = N;
+        self.Nx = Nx;
+        self.Nu = Nu;
+
+        self.zero = appx_zero;
+        self.step = grad_step;
+
+        # if nmax is -1 -> no iteration cap
+        self.nmax = max_iter;
+
+        self._alpha = 1;
+
+    def setAlpha(self, a):
+        self._alpha = a;
+        return;
+
+    def forward(self, x0, uinit, N=None, output=0):
+        # if N (prediction horizon) not given
+        if N is None:
+            # start alg. with class N
+            return self.forward(x0, uinit, self.N, output);
+
+        # if N = 1 end recursion by minimizing with terminal cost
+        if N == 1:
+            cost = lambda u: self.pcost(x0,u) + self.tcost( self.model(x0,u) );
+            ustar, Jstar = self.minimize(cost, uinit);
+            print(ustar);
+            return ustar, Jstar, self.model( x0,ustar );
+
+        cost = lambda u: self.pcost( x0,u ) + self.forward( self.model(x0, u), u, N=N-1 )[1];
+        ustar, Jstar = self.minimize(cost, uinit);
+        return ustar, Jstar;
+
+    def minimize(self, cost, uinit, h=1e-3):
+        un = [uinit[i] for i in range(self.Nu)];
+        gn = self.fdm(cost, un, h=self.step);
+        gnorm = sum([gn[i]**2 for i in range(self.Nu)]);
+
+        # gradient descent loop
+        count = 1;
+        while gnorm > self.zero**2:
+            un = [un[i] - self._alpha*gn[i] for i in range(self.Nu)];
+            gn = self.fdm(cost, un, h=self.step);
+            gnorm = sum([gn[i]**2 for i in range(self.Nu)]);
+
+            count += 1;
+            if (self.nmax != -1) and (count > self.nmax-1):  # iteration limit
+                print('WARNING: Iteration break in DynamicProgramming.minimize()');
+                break;
+
+        return un, cost(un);
+
+    def fdm(self, cost, u, h=1e-3):
+        dJ = [0 for i in range(self.Nu)];
+
+        for i in range(self.Nu):
+            un1 = [u[j] - (i==j)*h for j in range(self.Nu)];
+            up1 = [u[j] + (i==j)*h for j in range(self.Nu)];
+
+            Jn1 = cost(un1);
+            Jp1 = cost(up1);
+
+            dJ[i] = (Jp1 - Jn1)/(2*h);
+
+        return dJ;
+
 # model and cost functions
 def model(x, u):
     F     = u[0];
@@ -34,20 +110,20 @@ if __name__ == "__main__":
     x0 = [0,0,0,0,0];
 
     # create MPC class variable
-    N = 10;
+    N = 2;
     model_type = 'discrete';
     vhc = Vehicle(x0, xd);
-    dpvar = dpa.DynamicProgramming(pcost, tcost, model, N, dNx, Nu,
+    dpvar = DynamicProgramming(pcost, tcost, model, N, dNx, Nu,
         params=vhc, max_iter=-1);
-    dpvar.setAlpha(1);
+    dpvar.setAlpha(0.9);
 
     # test DPA
     uinit = [0 for i in range(Nu)]
     cost = lambda u: pcost(x0,u) + tcost( model(x0,u) );
-    ustar, Jstar = dpvar.minimize(cost, uinit);
+    ustar, Jstar = dpvar.forward(x0, uinit);
 
     print(ustar, Jstar);
-    print(model(x0,ustar));
+    print(model(x0, ustar));
 
 
     # T = np.array( sim_results[0] );
--- root.py
+++ root.py
@@ -1,8 +1,6 @@
 import sys
 sys.path.insert(0, '/home/michaelnaps/prog/mpc');
 
-
-import dpa
 import mpc
 
 import numpy as np
