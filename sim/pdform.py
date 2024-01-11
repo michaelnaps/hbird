01f5421ee6b3d34a2a0068a4a6ad52b421fca451
Michael Napoli <mikednapoli@gmail.com>
2024-01-10 20:45:34 -0500

    testing tilt control function

--- sim/pdform.py
+++ sim/pdform.py
@@ -1,23 +1,27 @@
 from root import *
 
-k = -np.array( [10, 2.5] )
+k = -10.0
+c = -2.50
+h = 1/5.00
+r = 1/1.00 # -1/250
 
 def control(x):
     u = np.array( [
-        k[0]*x[2] + k[1]*x[8],
-        k[0]*x[3] + k[1]*x[9],
-        k[0]*x[4] + k[1]*x[10],
-        k[0]*x[5] + k[1]*x[11]
+        k*x[2] + c*x[8],
+        (k*x[3] + c*x[9]) + (h*x[1] + r*x[7]),
+        (k*x[4] + c*x[10]) + (h*x[0] + r*x[6]),
+        k*x[5] + c*x[11]
     ] )
     return u
 
 if __name__ == '__main__':
     # Simulation length.
-    Nt = round( 60/dt ) + 1
+    T = 60;  Nt = round( T/dt ) + 1
     tlist = np.array( [i*dt for i in range( Nt )] )
 
     # Date set initialization.
-    Xlist = np.empty( (n,Nt) )
+    A = np.pi/2
+    Xlist = 2*A*np.random.rand( n,Nt ) - A
     Xlist[:,0] = np.ones( (n,) )
 
     # Simulation block.
@@ -26,8 +30,8 @@ if __name__ == '__main__':
         Xlist[:,i+1] = model( x, control( x ) )[:,0]
 
     # Plot simulation.
-    fig, axspos = plt.subplots( 3,2 )
-    fig, axsvel = plt.subplots( 3,2 )
+    fig, axspos = plt.subplots( 2,3 )
+    fig, axsvel = plt.subplots( 2,3 )
     i = 0
     for axsrow in axspos:
         for axs in axsrow:
